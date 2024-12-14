998fcbbee2294cba59d14d7bec97224ae801e348
BugHunter-X <askzenai@gmail.com>
2024-12-14 09:28:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  // Asynchronous operation that might throw an error
+  someAsyncOperation().then(() => {
+    res.send('Hello World!');
+  }).catch(error => {
+    // Error handling is missing here! The server crashes without any useful information 
+  });
+});
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+function someAsyncOperation() {
+  return new Promise((resolve, reject) => {
+    // Simulate an error
+    setTimeout(() => {
+      reject(new Error('Something went wrong!'));
+    }, 1000);
+  });
+}
